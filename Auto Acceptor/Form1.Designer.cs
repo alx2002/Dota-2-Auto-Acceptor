01f07c10d828efb57a3d24199092c44f5095f9d5
Alexander Gaiduk <alexandergy108@gmail.com>
2019-06-20 00:57:26 -0700

    Init

---
+++ Auto Acceptor/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Dota2AA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_toggleScan = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\"Begin Queue\" to enable the accept dialog.";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // btn_toggleScan
+            // 
+            this.btn_toggleScan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_toggleScan.Location = new System.Drawing.Point(23, 42);
+            this.btn_toggleScan.Name = "btn_toggleScan";
+            this.btn_toggleScan.Size = new System.Drawing.Size(266, 39);
+            this.btn_toggleScan.TabIndex = 1;
+            this.btn_toggleScan.Text = "Begin Queue";
+            this.btn_toggleScan.UseVisualStyleBackColor = true;
+            this.btn_toggleScan.Click += new System.EventHandler(this.btn_toggleScan_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_status.Location = new System.Drawing.Point(115, 84);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(83, 24);
+            this.lbl_status.TabIndex = 3;
+            this.lbl_status.Text = "Disabled";
+            this.lbl_status.Click += new System.EventHandler(this.Lbl_status_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(125, 111);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Stay on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 162);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_toggleScan);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dota 2 Auto Acceptor";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_toggleScan;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Auto Acceptor/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
+using CSCore.CoreAudioAPI;
+
+
+namespace Dota2AA
+{
+    public partial class Form1 : Form
+    {
+
+
+        IntPtr localhWnd;
+
+        Boolean scanning = false;
+
+        System.Timers.Timer checkTimer;
+        System.Timers.Timer delayTimer;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //Safe Windowhandle
+            localhWnd = Handle;
+
+            //Initialize the scanning timer
+            checkTimer = new System.Timers.Timer(1000);
+            checkTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent); //OnTimedEvent -> Gets called every interval.
+            checkTimer.Enabled = false;
+
+            //Initialize the delay timer for use after accepting to restart scanning
+            delayTimer = new System.Timers.Timer(5000);
+            delayTimer.Elapsed += (s, e) => { checkTimer.Enabled = true; Debug.WriteLine("DelayTimer Executed"); }; //Restart scanning
+            delayTimer.Elapsed += (s, e) => { ((System.Timers.Timer)s).Enabled = false; }; //Stop the delay timer (Only exec once)
+            delayTimer.Enabled = false;
+
+
+        }
+        
+
+        private static bool checkForSoundPeak() {
+
+
+            using (var sessionManager = GetDefaultAudioSessionManager2(DataFlow.Render))
+            {
+                using (var sessionEnum = sessionManager.GetSessionEnumerator())
+                {
+                    foreach (var session in sessionEnum)
+                    {
+                        using (var session2 = session.QueryInterface<AudioSessionControl2>())
+                        using (var audioMeterInformation = session.QueryInterface<AudioMeterInformation>())
+                        {
+
+                            //See if Dota emits sound (If there are two Windows containing Dota in the title sound of either one will trigger!!
+
+                            if (session2.Process.MainWindowTitle.Contains("Dota") && (audioMeterInformation.GetPeakValue() > 0.015))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
+        }
+
+
+
+        private void accept_match()
+        {
+            //Disable Scanning
+            checkTimer.Enabled = false;
+            
+            //Execute the Keystroke Simulator to submit enter
+            String appStartPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            var programPath = System.IO.Path.Combine(appStartPath, "keystroke_simulator.exe");
+            Debug.WriteLine(programPath);
+            ProcessStartInfo kssi = new ProcessStartInfo(programPath);
+            Process kss = Process.Start(kssi);
+            kss.WaitForExit(3000);
+
+            Debug.WriteLine("Keystokes sent");
+            SetForegroundWindow(localhWnd);
+
+            //Restart scanning after 5 seconds in case of declination
+            delayTimer.Enabled = true;  
+        }
+
+
+
+        private void btn_toggleScan_Click(object sender, EventArgs e)
+        {
+            //Search for the Dota 2 Process
+            Process p = Process.GetProcessesByName("dota2").FirstOrDefault();
+            if (p == null)
+            {
+                lbl_status.Text = "Dota 2 not found";
+                return;
+            };
+           
+
+            //Implement Toggle behaviour of the button
+            scanning = !scanning;
+            checkTimer.Enabled = scanning;
+            delayTimer.Enabled = false;
+            string btntxt = "Start";
+
+            if(scanning){
+
+                btntxt = "Stop";
+
+            }
+
+            btn_toggleScan.Text = btntxt;
+            string lbltxt = "Disabled";
+
+            if (scanning) 
+            { 
+                lbltxt = "Scanning";
+            }
+
+            lbl_status.Text = lbltxt;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (checkForSoundPeak())
+            {
+                accept_match();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_deviceStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox q = (CheckBox)sender;
+            TopMost = false;
+            if (q.Checked)
+            {
+                TopMost = true;
+            }
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
